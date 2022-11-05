6137c54727e0ade7ce22c15f8a7df98971c076fd
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-11-05 22:34:35 +0200

    3 files modified

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 const SignUp = () => {
 return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
+	<div>
 	<h1>Sign Up</h1>
 	</div>
 );
--- src/pages/team.js
+++ src/pages/team.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 const Teams = () => {
 return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
+	<div>
 	<h1>Welcome to GeeksforGeeks Team</h1>
 	</div>
 );
