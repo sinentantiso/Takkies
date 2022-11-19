3ebbbf38fe5d29a11597669fcbbe16e41fe1689f
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-19 19:54:14 +0200

    update

---
+++ src/takkiesData.js
@@ -0,0 +1,36 @@
+[{
+    "name": "some shoes",
+    "varients" : [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 500,
+            "medium" : 750,
+            "large" : 1000
+        }
+    ],
+    "catergory" : "nike",
+    "image" : "./images/20220809_075052",
+    "description": "some nike shoe"
+}, {
+    "name": "some shoes",
+    "varients" : [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 500,
+            "medium" : 750,
+            "large" : 1000
+        }
+    ],
+    "catergory" : "new-balance",
+    "image" : "./images/20220809_075108",
+    "description": "some New Balance shoe"
+}
+]
