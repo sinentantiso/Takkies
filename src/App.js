cc20d9d5311c9f47d38bd922da8bf96453a580e8
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-19 18:49:31 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Events from './pages/events';
-import AnnualReport from './pages/annual';
-import Teams from './pages/team';
-import Blogs from './pages/blogs';
-import SignUp from './pages/signup';
+import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
 
 function App() {
 return (
