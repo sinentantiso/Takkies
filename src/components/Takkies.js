88414b4e7f58631c2c6bb1ea18838d04ffab4c6d
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-19 20:18:01 +0200

    update

---
+++ src/components/Takkies.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import takkies from './../takkiesData';
+
+export default function Takkies({takkies}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/screens/Homescreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import takkies from '../takkiesData'
+
+export default function Homescreen() {
+  return (
+    <div>
+      <div className='row'>
+
+      </div>
+    </div>
+  )
+}
