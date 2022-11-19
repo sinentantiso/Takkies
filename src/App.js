0623b772313b69896781a3386d65da9131772fdc
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-19 19:17:53 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import './App.css';
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-
+import Navbar from "./components/Navbar"
 
 function App() {
 return (
-	<Router>
-	<Navbar />
-	<Routes>
-		<Route path='/' exact component={Home} />
-		<Route path='/about' component={About} />
-		<Route path='/events' component={Events} />
-		<Route path='/annual' component={AnnualReport} />
-		<Route path='/team' component={Teams} />
-		<Route path='/blogs' component={Blogs} />
-		<Route path='/sign-up' component={SignUp} />
-	</Routes>
-	</Router>
+	<div className="App">
+		<Navbar />
+	</div>
 );
 }
 
